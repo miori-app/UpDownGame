b2d28f9bc3e7fd508dfd0fc62c087a3f4f63bc31
iammiori <mioriii@naver.com>
2020-07-14 02:13:20 +0900

    [feat] Add reset fun and hit button implementaion

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -12,8 +12,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var randomValue = 0
+    var tryCnt : Int = 0
+    
     //storyboard에서 갖고와
+    /*
+     IBOutlet : 클래스의 프로퍼티와 인터페이스 빌더의 요소를 연결하기 위해 프로퍼티 앞에 붙이는 어노테이션(Annotation)
+     */
     @IBOutlet weak var slider : UISlider!
+    @IBOutlet weak var tryCntLabel : UILabel!
+    @IBOutlet weak var slierValueLabel : UILabel!
+    @IBOutlet weak var minValueLabel : UILabel!
+    @IBOutlet weak var maxValueLabel : UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,16 +32,23 @@ class ViewController: UIViewController {
         reset()
     }
     /*
-     @IBAction : 인터페이스 빌더에서 연결할 action function
+     @IBAction : 클래스의 액션 메서드와 인터페이스 빌더의 이벤트를 연결하기 위해 메서드 앞에 붙이는 어노테이션(Annotation)
      */
     
     //event driven
     @IBAction func sliderValueChanged( sender : UISlider){
         print(sender.value)
+        let integerValue = Int(sender.value)
+        slierValueLabel.text = String(integerValue)
     }
 
     @IBAction func touchUpHitButton( sender : UIButton){
         print(slider.value)
+        let hitValue = Int(slider.value)
+        slider.value = Float(hitValue)
+        tryCnt += 1
+        //tryCntLabel.text = String(tryCnt) + " / 5"
+        tryCntLabel.text = "\(tryCnt) / 5"
     }
     
     @IBAction func touchUpResetButton( sender : UIButton){
@@ -41,6 +58,19 @@ class ViewController: UIViewController {
     
     @IBAction func reset(){
         print("reset")
+        randomValue = Int.random(in: 0...30)
+        print(randomValue)
+        // ...은 범위 연산자. 폐쇠연산자(양쪽 다 포함)
+        //사용자 시도횟수 초기화
+        tryCnt = 0
+        tryCntLabel.text = "0 / 5"
+        slider.minimumValue = 0
+        slider.maximumValue = 30
+        slider.value = 15
+        minValueLabel.text = "0"
+        maxValueLabel.text = "30"
+        slierValueLabel.text = "15"
+        
     }
 }
 
